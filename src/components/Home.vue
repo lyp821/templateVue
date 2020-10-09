30c1570412cd95049f73a50c65dd89c72fdee90d
nine-git <15397725059@163.com>
2020-10-09 17:16:29 +0800

    feat:init

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    11
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      active: 0,
+    };
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
